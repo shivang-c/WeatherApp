08531400b853e72e50c9eda87c2b0ce0ccdc754f
Shivang Chopra <shivang.c@xebo.ai>
2024-04-19 13:02:26 +0530

    Refactor CurrentWeather.js and add RowText.js

--- src/screens/CurrentWeather.js
+++ src/screens/CurrentWeather.js
@@ -1,23 +1,32 @@
 import React from "react";
-import { Text, View, SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text, View, StyleSheet } from "react-native";
 import { Feather } from '@expo/vector-icons';
+import RowText from "../components/RowText";
 
 const CurrentWeather = () => {
+  const { wrapper, container, temp, feels, highLowWrapper, highLow, bodyWrapper, description, message } = styles;
   return(
-    <SafeAreaView style = {styles.wrapper}>
-      <View style = {styles.container}>
+    <SafeAreaView style = {wrapper}>
+      <View style = {container}>
         <Feather name = "sun" size = {100} color = "black" />
-        <Text style = {styles.temp}>6</Text>
-        <Text style = {styles.feels}>Feels like: 5</Text>
-        <View style = {styles.highLowWrapper}>
-          <Text style = {styles.highLow}>High: 8 </Text>
-          <Text style = {styles.highLow}>Low: 6</Text>
-        </View>
+        <Text style = {temp}>6</Text>
+        <Text style = {feels}>Feels like: 5</Text>
+        <RowText 
+          messageOne = {"High: 8"} 
+          messageTwo = {"Low: 6"} 
+          containerStyles = {highLowWrapper} 
+          messageOneStyles = {highLow} 
+          messageTwoStyles = {highLow} 
+        />
       </View>
-      <View style = {styles.bodyWrapper}>
-        <Text style = {styles.description}>It's sunny</Text>
-        <Text style = {styles.message}>It's perfect t-shirt weather</Text>
-      </View>
+      <RowText 
+        messageOne = {"It's sunny"} 
+        messageTwo = {"It's perfect t-shirt weather"} 
+        containerStyles = {bodyWrapper} 
+        messageOneStyles = {description}
+        messageTwoStyles = {message} 
+      />
     </SafeAreaView>
   )
 }
