d82c82ad2b2b013d8b23b09a9a65dfa3daf59e6e
Shivang Chopra <shivang.c@xebo.ai>
2024-04-19 13:42:54 +0530

    Refactor App.js and create Tab.js

--- src/screens/CurrentWeather.js
+++ src/screens/CurrentWeather.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Text, View, StyleSheet, SafeAreaView } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text, View, StyleSheet } from "react-native";
 import { Feather } from '@expo/vector-icons';
 import RowText from "../components/RowText";
 
